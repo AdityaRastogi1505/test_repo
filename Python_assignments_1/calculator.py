cabda6e34514d64c1d97466a081fb3e901fa16d6
Aditya <aditya@example.com>
2025-08-20 23:58:49 +0530

    assignment1

---
+++ Python_assignments_1/calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first no: "))
+b = int(input("Enter second no: "))
+
+print("Addition: ", a+b)
+print("Substraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
---
+++ Python_assignments_1/greeting.py
@@ -0,0 +1,4 @@
+n1 = input("Enter you first name: ")
+n2 = input("ENter your last name: ")
+
+print("Hello " + n1 + " " + n2 + "! Welcome to the Python Program.")
